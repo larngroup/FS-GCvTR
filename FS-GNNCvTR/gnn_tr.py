af6e5b96710bda3567a10cc5736e72f412d482d5
ltorres97 <luistorres97@hotmail.com>
2022-12-28 23:08:18 +0000

    First commit

---
+++ FS-GNNCvTR/gnn_tr.py
@@ -0,0 +1,233 @@
+import torch
+from torch import nn, einsum
+import torch.nn.functional as F
+from torch_geometric.nn import global_mean_pool
+from gnn_models import GNN
+from torch_geometric.nn import MessagePassing
+from einops import rearrange
+from torch_geometric import utils
+import torch_geometric.utils as utils
+from torch_scatter import scatter_add, scatter_mean, scatter_max
+import torch_geometric.nn as gnn
+
+from einops import rearrange, repeat
+from einops.layers.torch import Rearrange
+
+# helper methods
+
+def group_dict_by_key(cond, d):
+    return_val = [dict(), dict()]
+    for key in d.keys():
+        match = bool(cond(key))
+        ind = int(not match)
+        return_val[ind][key] = d[key]
+    return (*return_val,)
+
+def group_by_key_prefix_and_remove_prefix(prefix, d):
+    kwargs_with_prefix, kwargs = group_dict_by_key(lambda x: x.startswith(prefix), d)
+    kwargs_without_prefix = dict(map(lambda x: (x[0][len(prefix):], x[1]), tuple(kwargs_with_prefix.items())))
+    return kwargs_without_prefix, kwargs
+
+# classes
+
+class LayerNorm(nn.Module): # layernorm, but done in the channel dimension #1
+    def __init__(self, dim, eps = 1e-5):
+        super().__init__()
+        self.eps = eps
+        self.g = nn.Parameter(torch.ones(1, dim, 1, 1))
+        self.b = nn.Parameter(torch.zeros(1, dim, 1, 1))
+
+    def forward(self, x):
+        var = torch.var(x, dim = 1, unbiased = False, keepdim = True)
+        mean = torch.mean(x, dim = 1, keepdim = True)
+        return (x - mean) / (var + self.eps).sqrt() * self.g + self.b
+
+class PreNorm(nn.Module):
+    def __init__(self, dim, fn):
+        super().__init__()
+        self.norm = LayerNorm(dim)
+        self.fn = fn
+    def forward(self, x, **kwargs):
+        x = self.norm(x)
+        return self.fn(x, **kwargs)
+
+class FeedForward(nn.Module):
+    def __init__(self, dim, mult = 4, dropout = 0.):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Conv2d(dim, dim * mult, 1),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Conv2d(dim * mult, dim, 1),
+            nn.Dropout(dropout)
+        )
+    def forward(self, x):
+        return self.net(x)
+
+class DepthWiseConv2d(nn.Module):
+    def __init__(self, dim_in, dim_out, kernel_size, padding, stride, bias = True):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Conv2d(dim_in, dim_in, kernel_size = kernel_size, padding = padding, groups = dim_in, stride = stride, bias = bias),
+            nn.BatchNorm2d(dim_in),
+            nn.Conv2d(dim_in, dim_out, kernel_size = 1, bias = bias)
+        )
+    def forward(self, x):
+        return self.net(x)
+
+class Attention(nn.Module):
+    def __init__(self, dim, proj_kernel, kv_proj_stride, heads = 8, dim_head = 64, dropout = 0.):
+        super().__init__()
+        inner_dim = dim_head *  heads
+        padding = proj_kernel // 2
+        self.heads = heads
+        self.scale = dim_head ** -0.5
+
+        self.attend = nn.Softmax(dim = -1)
+        self.dropout = nn.Dropout(dropout)
+
+        self.to_q = DepthWiseConv2d(dim, inner_dim, proj_kernel, padding = padding, stride = 1, bias = False)
+        self.to_kv = DepthWiseConv2d(dim, inner_dim * 2, proj_kernel, padding = padding, stride = kv_proj_stride, bias = False)
+
+        self.to_out = nn.Sequential(
+            nn.Conv2d(inner_dim, dim, 1),
+            nn.Dropout(dropout)
+        )
+
+    def forward(self, x):
+        shape = x.shape
+        b, n, _, y, h = *shape, self.heads
+        q, k, v = (self.to_q(x), *self.to_kv(x).chunk(2, dim = 1))
+        q, k, v = map(lambda t: rearrange(t, 'b (h d) x y -> (b h) (x y) d', h = h), (q, k, v))
+
+        dots = einsum('b i d, b j d -> b i j', q, k) * self.scale
+
+        attn = self.attend(dots)
+        attn = self.dropout(attn)
+
+        out = einsum('b i j, b j d -> b i d', attn, v)
+        out = rearrange(out, '(b h) (x y) d -> b (h d) x y', h = h, y = y)
+        return self.to_out(out)
+
+class Transformer(nn.Module):
+    def __init__(self, dim, proj_kernel, kv_proj_stride, depth, heads, dim_head = 64, mlp_mult = 4, dropout = 0.):
+        super().__init__()
+        self.layers = nn.ModuleList([])
+        for _ in range(depth):
+            self.layers.append(nn.ModuleList([
+                PreNorm(dim, Attention(dim, proj_kernel = proj_kernel, kv_proj_stride = kv_proj_stride, heads = heads, dim_head = dim_head, dropout = dropout)),
+                PreNorm(dim, FeedForward(dim, mlp_mult, dropout = dropout))
+            ]))
+    def forward(self, x):
+        for attn, ff in self.layers:
+            x = attn(x) + x
+            x = ff(x) + x
+        return x
+
+class ConvTR(nn.Module):
+    def __init__(
+        self,
+        num_classes = 1,
+        s1_emb_dim = 32,
+        s1_emb_kernel = 7,
+        s1_emb_stride = 4,
+        s1_proj_kernel = 3,
+        s1_kv_proj_stride = 2,
+        s1_heads = 1,
+        s1_depth = 1,
+        s1_mlp_mult = 4,
+        s2_emb_dim = 64,
+        s2_emb_kernel = 3,
+        s2_emb_stride = 2,
+        s2_proj_kernel = 3,
+        s2_kv_proj_stride = 2,
+        s2_heads = 3,
+        s2_depth = 2,
+        s2_mlp_mult = 4,
+        s3_emb_dim = 128,
+        s3_emb_kernel = 3,
+        s3_emb_stride = 2,
+        s3_proj_kernel = 3,
+        s3_kv_proj_stride = 2,
+        s3_heads = 6,
+        s3_depth = 10,
+        s3_mlp_mult = 4,
+        dropout = 0.
+    ):
+        super().__init__()
+        kwargs = dict(locals())
+
+        dim = 1
+        layers = []
+
+        for prefix in ('s1', 's2', 's3'):
+            config, kwargs = group_by_key_prefix_and_remove_prefix(f'{prefix}_', kwargs)
+
+            layers.append(nn.Sequential(
+                nn.Conv2d(dim, config['emb_dim'], kernel_size = config['emb_kernel'], padding = (config['emb_kernel'] // 2), stride = config['emb_stride']),
+                LayerNorm(config['emb_dim']),
+                Transformer(dim = config['emb_dim'], proj_kernel = config['proj_kernel'], kv_proj_stride = config['kv_proj_stride'], depth = config['depth'], heads = config['heads'], mlp_mult = config['mlp_mult'], dropout = dropout)
+            ))
+
+            dim = config['emb_dim']
+
+        self.layers = nn.Sequential(*layers)
+
+        self.to_logits = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),
+            Rearrange('... () () -> ...'),
+            nn.Linear(dim, num_classes)
+        )
+
+    def forward(self, x):
+        
+        emb = x.reshape(10,1,300,1)
+        
+        emb = self.layers(emb)
+        
+        return self.to_logits(emb)
+        
+class GNN_prediction(torch.nn.Module):
+
+    def __init__(self, layer_number, emb_dim, jk = "last", dropout_prob= 0, pooling = "mean", gnn_type = "gin"):
+        super(GNN_prediction, self).__init__()
+        
+        self.num_layer = layer_number
+        self.drop_ratio = dropout_prob
+        self.jk = jk
+        self.emb_dim = emb_dim
+
+        if self.num_layer < 2:
+            raise ValueError("Number of layers must be > 2.")
+
+        self.gnn = GNN(layer_number, emb_dim, jk, dropout_prob, gnn_type = gnn_type)
+        
+        if pooling == "mean":
+            self.pool = global_mean_pool
+        else:
+            raise ValueError("Invalid pooling.")
+
+        self.mult = 1
+        self.graph_pred_linear = torch.nn.Linear(self.mult * self.emb_dim, 1)
+        
+    def from_pretrained(self, model_file):
+        self.gnn.load_state_dict(torch.load(model_file, map_location='cuda:0'), strict = False)
+        
+    def forward(self, *argv):
+        if len(argv) == 4:
+            x, edge_index, edge_attr, batch = argv[0], argv[1], argv[2], argv[3]
+        elif len(argv) == 1:
+            data = argv[0]
+            x, edge_index, edge_attr, batch = data.x, data.edge_index, data.edge_attr, data.batch
+        else:
+            raise ValueError("The arguments are unmatched!")
+
+        node_embeddings = self.gnn(x, edge_index, edge_attr)
+            
+        pred_gnn = self.graph_pred_linear(self.pool(node_embeddings, batch))
+        
+        return pred_gnn, node_embeddings
+        
+        
+if __name__ == "__main__":
+    pass
---
+++ README.md
@@ -0,0 +1,112 @@
+## Few-shot Learning with Convolutional Transformers via Graph Embeddings for Toxicity and Side Effect Prediction
+
+In this work, we present a few-shot GNN-Transformer architecture called FS-GNNCvTR that addresses the challenge of low data learning in toxicity and side effect prediction.  It is demonstrated that FS-GNNCvTR outperforms simpler graph-based methods on benckmarks datasets such as Tox21 and SIDER.
+
+First, a GNN converts the topological structure of molecular graphs into molecular graph embeddings using neighborhood aggregation. A convolutional Transformer encoder exploits the contextual information of these vectorial embeddings to propagate deep representations across self-attention layers. The Transformer includes a convolutional component to reconcile the local information captured by convolutional filters with the global dependencies preserved by the dynamic attention operations of Transformer networks.
+
+![ScreenShot](plots/fs-gnncvtr.png?raw=true)
+
+A meta-learning framework was explored to optimize model parameters across tasks and quickly adapt to new molecular properties on few-shot data. 
+
+Extensive experiments on real multiproperty prediction data demonstrate the predictive power and stable performances of the proposed model when inferring specific toxicity and side effect properties adaptively.
+
+This repository provides the source code and datasets for the proposed work.
+
+Contact Information: (uc2015241578@student.uc.pt, luistorres@dei.uc.pt), if you have any questions about this work.
+
+## Data Availability and Pre-Processing
+
+The Tox21 and SIDER datasets are downloaded from [Data](http://snap.stanford.edu/gnn-pretrain/data/) (chem_dataset.zip). 
+
+The script loader.py pre-processes the data and transforms SMILES strings into molecular graphs using RDKit.Chem. 
+
+Data pre-processing and pre-trained models are implemented based on [Strategies for Pre-training Graph Neural Networks (Hu et al.) (2020)](https://arxiv.org/abs/1905.12265).
+
+## Code Usage
+
+### Installation
+We used the following Python packages for core development. We tested on Python 3.7.
+
+```
+- torch = 1.10.1
+- torch-cluster = 1.5.9
+- torch-geometric = 2.0.4
+- torch-scatter = 2.0.9
+- torch-sparse = 0.6.12
+- torch-spline-conv = 1.2.1
+- torchvision = 0.10.0
+- vit-pytorch = 0.35.8
+- scikit-learn = 1.0.2
+- seaborn = 0.11.2
+- scipy = 1.8.0
+- numpy = 1.21.5
+- tqdm = 4.50.0
+- tensorflow = 2.8.0
+- keras = 2.8.0
+- tsnecuda = 3.0.1
+- tqdm = 4.62.3
+- matplotlib = 3.5.1
+- pandas = 1.4.1
+- networkx = 2.7.1
+- rdkit
+```
+
+## References
+
+[1] Hu, W., Liu, B., Gomes, J., Zitnik, M., Liang, P., Pande, V., Leskovec, J.: Strategies for pre-training graph neural networks. CoRR abs/1905.12265 (2020). https://doi.org/10.48550/ARXIV.1905.12265
+
+```
+@inproceedings{
+hu2020pretraining,
+title={Strategies for Pre-training Graph Neural Networks},
+author={Hu, Weihua and Liu, Bowen and Gomes, Joseph and Zitnik, Marinka and Liang, Percy and Pande, Vijay and Leskovec, Jure},
+booktitle={International Conference on Learning Representations},
+year={2020},
+url={https://openreview.net/forum?id=HJlWWJSFDH},
+}
+
+```
+
+[2] Finn, C., Abbeel, P., Levine, S.: Model-agnostic meta-learning for fast adaptation of deep networks. In: 34th International Conference on Machine Learning, ICML 2017, vol. 3 (2017). https://doi.org/10.48550/arXiv.1703.03400
+
+```
+@article{finn17maml,
+  author    = {Chelsea Finn and Pieter Abbeel and Sergey Levine},
+  title     = {{Model-Agnostic Meta-Learning for Fast Adaptation of Deep Networks}},
+  journal   = {International Conference on Machine Learning (ICML)},
+  year      = {2017},
+  url       = {http://arxiv.org/abs/1703.03400}
+}
+
+```
+
+[3] Wu, H., Xiao, B., Codella, N., Liu, M., Dai, X., Yuan, L., Zhang, L. (2021). CvT: Introducing Convolutions to Vision Transformers. Proceedings of the IEEE International Conference on Computer Vision, 22–31. https://doi.org/10.1109/ICCV48922.2021.00009
+
+```
+@inproceedings{Wu2021,
+   author = {Haiping Wu and Bin Xiao and Noel Codella and Mengchen Liu and Xiyang Dai and Lu Yuan and Lei Zhang},
+   journal = {Proceedings of the IEEE International Conference on Computer Vision},
+   pages = {22-31},
+   publisher = {Institute of Electrical and Electronics Engineers Inc.},
+   title = {CvT: Introducing Convolutions to Vision Transformers},
+   year = {2021},
+}
+
+
+```
+[4] Vision Transformers with PyTorch. https://github.com/lucidrains/vit-pytorch
+
+```
+@misc{Phil Wang,
+  author = {Phil Wang},
+  title = {Vision Transformers},
+  year = {2021},
+  publisher = {GitHub},
+  journal = {GitHub repository},
+  howpublished = {\url{https://github.com/lucidrains/vit-pytorch}},
+}
+
+```
+
+
+
