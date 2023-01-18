a33e7c6e1610a1814f9ec9adccfd00e7d50cc9d7
ltorres97 <42170137+ltorres97@users.noreply.github.com>
2023-01-18 12:04:29 +0000

    Update eval_model.py

--- FS-GNNCvTR/eval_model.py
+++ FS-GNNCvTR/eval_model.py
@@ -13,7 +13,7 @@ def save_ckp(state, is_best, checkpoint_dir, best_model_dir, filename, best_mode
     #    best_fpath = best_model_dir + best_model
     #    shutil.copyfile(f_path, best_fpath)
 
-def save_result(epoch, N, exp, filename, tl):
+def save_result(epoch, N, exp, filename):
             
     file = open(filename, "a")
     
@@ -22,52 +22,39 @@ def save_result(epoch, N, exp, filename, tl):
         file.write(str(exp) + "\t")
     if epoch == N:
         file.write(str(exp))
-        if tl == 0:
-            for list_acc in exp:
-                file.write(str(epoch) + " Support Sets: (Mean:"+ str(statistics.mean(list_acc)) +", SD:" +str(statistics.stdev(list_acc)) + ") | \t")
-        elif tl == 1:
-            file.write(str(epoch) + " Support Sets: (Mean:"+ str(statistics.mean(exp[0])) +", SD:" +str(statistics.stdev(exp[0])) + ") | \t")
-            
-          
+        for list_acc in exp:
+           file.write(str(epoch) + " Support Sets: (Mean:"+ str(statistics.mean(list_acc)) +", SD:" +str(statistics.stdev(list_acc)) + ") | \t")
+                 
     file.write("\n")
     file.close()
 
-
 dataset = "tox21"
-gnn= "gin"
+gnn = "gin"
 support_set = 5
 pretrained = "pre-trained/supervised_contextpred.pth"
-baseline = 1
+baseline = 0
 device = "cuda:0"
-tl = 0
 
 # FS-GNNCvTR - Two module GNN-CvTR architecture
-# GraphSage - assumes that nodes that reside in the same neighborhood should have similar embeddings.
+# GraphSage - Inductive method to aggregate node representations for unseen graph features
 # GIN - Graph Isomorphism Network
 # GCN - Standard Graph Convolutional Network
 
 device = "cuda:0"      
-model_eval = GNNCvTR_eval(dataset, gnn, support_set, pretrained, baseline, tl)
+model_eval = GNNCvTR_eval(dataset, gnn, support_set, pretrained, baseline)
 model_eval.to(device)
 
 print("Dataset:", dataset)
 
 roc_auc_list = []
 
-if tl == 0:
-    if dataset== "tox21":
-        exp = [[],[],[]]
-        labels =  ['SR-HSE', 'SR-MMP', 'SR-p53']
-    elif dataset == "sider":
-        exp = [[],[],[],[],[],[]]
-        labels =  ['R.U.D.', 'P.P.P.C.', 'E.L.D.', 'C.D.', 'N.S.D.', 'I.P.P.C.']    
+if dataset== "tox21":
+    exp = [[],[],[]]
+    labels =  ['SR-HSE', 'SR-MMP', 'SR-p53']
+elif dataset == "sider":
+    exp = [[],[],[],[],[],[]]
+    labels =  ['R.U.D.', 'P.P.P.C.', 'E.L.D.', 'C.D.', 'N.S.D.', 'I.P.P.C.']    
 
-if tl == 1:
-   exp = [[]]
-   labels = ['Mean_Task']
-   
-    
-    
 N = 30
    
 for epoch in range(1, 10000):
@@ -75,6 +62,7 @@ for epoch in range(1, 10000):
     roc_scores, gnn_model, transformer_model, gnn_opt, t_opt = model_eval.meta_evaluate() #FS-GNNCvTR
    
     #roc_scores, gnn_model, gnn_opt = model.meta_evaluate(grads) #baselines
+    
     if roc_auc_list != []:
         for score in range(len(roc_auc_list)):
 
@@ -95,25 +83,14 @@ for epoch in range(1, 10000):
     else:
         roc_auc_list = roc_scores
       
-    
-    save_result(epoch, N, exp, "results-exp/mean-GIN_tox21_5.txt", tl)
-    
-    if tl == 0:
-        if dataset == "tox21":
-            box_plot_data=[exp[0], exp[1], exp[2]]
-            plot_title = "Tox21"
-        elif dataset == "sider":
-            box_plot_data=[exp[0], exp[1], exp[2], exp[3], exp[4], exp[5]]
-            plot_title = "SIDER"  
-    
-    if tl == 1:
-        if dataset == "tox21":
-            box_plot_data=[exp[0]]
-            plot_title = "Transfer-Learning (SIDER->Tox21)"
-        elif dataset == "sider":
-            box_plot_data=[exp[0]]
-            plot_title = "Transfer-Learning (Tox21->SIDER)"
-
+    save_result(epoch, N, exp, "results-exp/mean-FSGNNCvTR_tox21_5.txt")
+      
+    if dataset == "tox21":
+        box_plot_data=[exp[0], exp[1], exp[2]]
+        plot_title = "Tox21"
+    elif dataset == "sider":
+        box_plot_data=[exp[0], exp[1], exp[2], exp[3], exp[4], exp[5]]
+        plot_title = "SIDER"  
     
     fig = plt.figure()   
     fig.suptitle(plot_title, fontsize=14, fontweight='bold')
@@ -122,7 +99,7 @@ for epoch in range(1, 10000):
     ax.set_xlabel('Test Task')
     ax.set_ylabel('ROC-AUC score')
     plt.grid(b=False)
-    if epoch == N and tl == 0:
+    if epoch == N:
         plt.savefig('plots/test/boxplot_GT_'+ str(dataset) + '_' + str(support_set), dpi=300)
     #plt.savefig('plots/boxplot_GT_'+ str(dataset) + '_' + str(support_set), dpi=300)
     plt.show()
